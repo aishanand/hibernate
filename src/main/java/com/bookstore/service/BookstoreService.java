da30d94dd18758e6d6d92ebb34cba8df2d1fc1b0
aravichandran <aravichandran@loc.gov>
2025-01-24 12:53:09 -0500

    update author name

--- src/main/java/com/bookstore/service/BookstoreService.java
+++ src/main/java/com/bookstore/service/BookstoreService.java
@@ -1,13 +1,17 @@
 package com.bookstore.service;
 
-import com.bookstore.repository.BookRepository;
-import com.bookstore.repository.AuthorRepository;
-import com.bookstore.entity.Author;
-import com.bookstore.entity.Book;
+
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.bookstore.entity.Author;
+import com.bookstore.entity.Book;
+import com.bookstore.repository.AuthorRepository;
+import com.bookstore.repository.BookRepository;
+
 
 @Service
 public class BookstoreService {
@@ -15,17 +19,19 @@ public class BookstoreService {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
 
-    public BookstoreService(AuthorRepository authorRepository,
-            BookRepository bookRepository) {
+
+    public BookstoreService(AuthorRepository authorRepository, BookRepository bookRepository) {
 
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
     }
 
+
     public List<Book> fetchBooksOfAuthor(String name) {
 
         return bookRepository.booksOfAuthor(name);
     }
+
 
     @Transactional
     public void updateBooksOfAuthor(String name, List<Book> detachedBooks) {
@@ -35,28 +41,65 @@ public class BookstoreService {
 
         // Remove the existing database rows that are no 
         // longer found in the incoming collection (detachedBooks)
-        List<Book> booksToRemove  = author.getBooks().stream()
-                .filter(b -> !detachedBooks.contains(b))
-                .toList();
-        booksToRemove .forEach(author::removeBook);
+        List<Book> booksToRemove = author.getBooks().stream().filter(b -> !detachedBooks.contains(b)).toList();
+        booksToRemove.forEach(author::removeBook);
 
         // Update the existing database rows which can be found 
         // in the incoming collection (detachedBooks)
-        List<Book> newBooks = detachedBooks.stream()
-                .filter(b -> !author.getBooks().contains(b))
-                .toList();
+        List<Book> newBooks = detachedBooks.stream().filter(b -> !author.getBooks().contains(b)).toList();
 
-        detachedBooks.stream()
-                .filter(b -> !newBooks.contains(b))
-                .forEach((b) -> {
-                    b.setAuthor(author);
-                    Book mergedBook = bookRepository.save(b);
-                    author.getBooks().set(
-                            author.getBooks().indexOf(mergedBook), mergedBook);
-                });
+        detachedBooks.stream().filter(b -> !newBooks.contains(b)).forEach((b) -> {
+            b.setAuthor(author);
+            Book mergedBook = bookRepository.save(b);
+            author.getBooks().set(author.getBooks().indexOf(mergedBook), mergedBook);
+        });
 
         // Add the rows found in the incoming collection, 
         // which cannot be found in the current database snapshot
         newBooks.forEach(b -> author.addBook(b));
     }
+
+
+    @Transactional
+    public Author updateAuthorName(final Long authorChangedId, final String newAuthorName) {
+
+        Author saved = null;
+
+        Optional<Author> dbAuthorOptional = authorRepository.findById(authorChangedId);
+
+        if (dbAuthorOptional.isPresent()) {
+
+            Author dbAuthor = dbAuthorOptional.get();
+            dbAuthor.setName(newAuthorName);
+
+            // this is merge
+            saved = authorRepository.save(dbAuthor);
+
+        }
+
+        return saved;
+    }
+
+
+    @Transactional
+    public Author updateAuthorNameWithIncoming(final Author newAuthor) {
+
+        // this is merge
+        return authorRepository.save(newAuthor);
+
+    }
+
+
+    public Author findAuthorById(final Long id) {
+
+        Author found = null;
+
+        Optional<Author> author = authorRepository.findById(id);
+        if (author.isPresent()) {
+            found = author.get();
+        }
+        return found;
+    }
+
+
 }
